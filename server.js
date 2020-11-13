8f90825690feba541628e3f7ad7403ed248d99bc
Lusidave <lusidave@hotmail.fr>
2020-11-13 11:20:06 +0100

    add query, add mutation

---
+++ server.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let express_graphql = require('express-graphql');
+let { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+// GraphQL schema
+let schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+// Root resolver
+let root = {
+    message: () => 'Hello World!'
+};
+
+// Create an express server and a GraphQL endpoint
+let app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
